705d6272b2ffe63b8bb21a18156020cec8054ca4
zreonx <zrodayrit@gmail.com>
2025-09-29 09:05:37 +0800

    added product sample data and product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const HomePage = async () => {
+  console.log(sampleData);
   return (
     <>
-      <Button>Button</Button>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
     </>
   );
 };
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,30 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {limitedData.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((product: any) => (
+            <div key={product.name}>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
