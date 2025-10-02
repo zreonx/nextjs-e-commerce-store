83b908c34189aef887d8afb1d2fa7fb6e3e2fea1
zreonx <zrodayrit@gmail.com>
2025-10-02 10:36:00 +0800

    added neon serverless and prisma instance config

---
+++ db/prisma.ts
@@ -0,0 +1,38 @@
+import { neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@/lib/generated/prisma';
+import ws from 'ws';
+import 'dotenv/config'
+
+// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+neonConfig.poolQueryViaFetch = true;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// Type definitions
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const adapter = new PrismaNeon({connectionString});
+
+// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+export const prisma =  global.prisma || new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,12 +1,11 @@
 "use server";
 
-import { PrismaClient } from "../generated/prisma";
+import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { convertToPlainObject } from "../utils";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
