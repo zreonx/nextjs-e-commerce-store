eaa6274a384d3bf2e17eb3c2b260f6ebb8d27be4
zreonx <zrodayrit@gmail.com>
2025-10-03 12:59:51 +0800

    added view product details by slug

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,18 +1,22 @@
-"use server";
+'use server';
 
-import { prisma } from "@/db/prisma";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
-import { convertToPlainObject } from "../utils";
+import { prisma } from '@/db/prisma';
+import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { convertToPlainObject } from '../utils';
 
 // Get latest products
 export async function getLatestProducts() {
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
-      createdAt: "desc",
+      createdAt: 'desc',
     },
   });
 
   return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({ where: { slug } });
 }
