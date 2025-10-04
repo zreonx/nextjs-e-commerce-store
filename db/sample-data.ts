8ecf4255e6125f09a9e04a6eeba58a03178a1e5d
zreonx <zrodayrit@gmail.com>
2025-10-04 18:09:57 +0800

    added user related table and seed user table

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from "bcrypt-ts-edge";
+
 const sampleData = {
+  users: [
+    {
+      name: 'Zreon',
+      email: 'admin@example.com',
+      password: hashSync('123456', 10),
+      role: 'admin'
+    },
+    {
+      name: 'Zreon',
+      email: 'user@example.com',
+      password: hashSync('123456', 10),
+      role: 'user'
+    }
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
--- db/seed.ts
+++ db/seed.ts
@@ -4,9 +4,17 @@ import sampleData from "./sample-data";
 async function main() {
   const prisma = new PrismaClient();
 
+  // delete table records
   await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.session.deleteMany();
 
+
+  // populate table with data
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
