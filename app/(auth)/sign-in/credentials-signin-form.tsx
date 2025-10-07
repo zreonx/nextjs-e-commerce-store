dcfb87214ca506a019ae2618bcc734fa0a82fd34
zreonx <zrodayrit@gmail.com>
2025-10-07 11:17:20 +0800

    added callback url function

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,55 +1,65 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Link from 'next/link';
-import { useActionState } from 'react';
-import { useFormStatus } from 'react-dom';
-import { signInWithCredentials } from '@/lib/actions/user.action';
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useActionState, useState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.action";
+import { useSearchParams } from "next/navigation";
 
 export const signInDefaultValues = {
-  email: '',
-  password: '',
+  email: "",
+  password: "",
 };
 
 const CredentialsSignInForm = () => {
+  const [email, setEmail] = useState("");
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
-    message: '',
+    message: "",
   });
+
+  const searchParams = useSearchParams(); // use to access search params on client component
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  console.log("client search params", callbackUrl)
 
   const SignInButton = () => {
     const { pending } = useFormStatus();
 
     return (
-      <Button disabled={pending} className="w-full" variant="default">
-        {pending ? 'Signing In...' : 'Sign In'}
+      <Button disabled={pending} className='w-full' variant='default'>
+        {pending ? "Signing In..." : "Sign In"}
       </Button>
     );
   };
 
   return (
     <form action={action}>
-      <div className="space-y-6">
-        <div className="space-y-2">
-          <Label htmlFor="email">Email</Label>
+      <input type='hidden' name='callbackUrl' value={callbackUrl} />
+      <div className='space-y-6'>
+        <div className='space-y-2'>
+          <Label htmlFor='email'>Email</Label>
           <Input
-            id="email"
-            name="email"
-            type="email"
+            id='email'
+            name='email'
+            type='email'
             required
-            autoComplete="email"
-            defaultValue={signInDefaultValues.email}
+            autoComplete='email'
+            // defaultValue={signInDefaultValues.email}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="space-y-2">
-          <Label htmlFor="password">Password</Label>
+        <div className='space-y-2'>
+          <Label htmlFor='password'>Password</Label>
           <Input
-            id="password"
-            name="password"
-            type="password"
+            id='password'
+            name='password'
+            type='password'
             required
-            autoComplete="password"
+            autoComplete='password'
             defaultValue={signInDefaultValues.password}
           />
         </div>
@@ -58,12 +68,12 @@ const CredentialsSignInForm = () => {
         </div>
 
         {data && !data.success && (
-          <div className="text-center text-destructive">{data.message}</div>
+          <div className='text-center text-destructive'>{data.message}</div>
         )}
 
-        <div className="text-sm text-center text-muted-foreground">
+        <div className='text-sm text-center text-muted-foreground'>
           Don&apos;t have an account?
-          <Link href="/sign-up" target="_self" className="link">
+          <Link href='/sign-up' target='_self' className='link'>
             Sign up
           </Link>
         </div>
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,44 +4,50 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { APP_NAME } from '@/lib/constants';
-import type { Metadata } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-import CredentialsSignInForm from './credentials-signin-form';
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: 'Sign In',
+  title: "Sign In",
 };
 
-const SignInPage = async () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) => {
+  const { callbackUrl } = await props.searchParams; // used to access search params on server component
+
+  // http://localhost:3000/sign-in?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fshipping-address
+
   const session = await auth();
 
   if (session) {
-    return redirect('/');
+    return redirect(callbackUrl || "/");
   }
 
   return (
-    <div className="w-full max-w-md mx-auto">
+    <div className='w-full max-w-md mx-auto'>
       <Card>
-        <CardHeader className="space-y-4">
-          <Link href="/" className="flex-center">
+        <CardHeader className='space-y-4'>
+          <Link href='/' className='flex-center'>
             <Image
-              src="/images/logo.svg"
+              src='/images/logo.svg'
               width={100}
               height={100}
               alt={`${APP_NAME} priority`}
             />
           </Link>
-          <CardTitle className="text-center">Sign In</CardTitle>
-          <CardDescription className="text-center">
+          <CardTitle className='text-center'>Sign In</CardTitle>
+          <CardDescription className='text-center'>
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
+        <CardContent className='space-y-4'>
           {/* FORM HERE */}
           <CredentialsSignInForm />
         </CardContent>
