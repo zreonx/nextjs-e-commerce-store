b035bc603d4fc090ce15a7d5d8f3be95c6b88ad7
zreonx <zrodayrit@gmail.com>
2025-10-16 11:06:36 +0800

    added add to cart actions

--- lib/actions/cart.action.ts
+++ lib/actions/cart.action.ts
@@ -1,10 +1,78 @@
 "use server";
 
 import { CartItem } from "@/types";
+import { cookies } from "next/headers";
+import { convertToPlainObject, formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    success: true,
-    message: "Item added to cart",
-  };
+  try {
+    // Check for cart cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Cart session not found");
+
+    // Get session user id
+    const session = await auth();
+    const userId: string | undefined = session?.user?.id;
+
+    // Get Cart
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: {
+        id: item.productId,
+      },
+    });
+
+    // Testing
+    console.log({
+      "Session Cart Id": sessionCartId,
+      "User id": userId,
+      "Item Requested": item,
+      "Product Found": product,
+    });
+
+    return {
+      success: true,
+      message: "Item added to cart",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  // Check for cart cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Cart session not found");
+
+  // Get session user id
+  const session = await auth();
+  const userId: string | undefined = session?.user?.id;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // Convert decimals and return
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString,
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
