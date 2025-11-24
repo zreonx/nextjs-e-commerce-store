4cd23e2365a2d6ee33fa085c84201ede677c9d13
zreonx <zrodayrit@gmail.com>
2025-11-24 14:43:42 +0800

    added next auth protected route

--- auth.ts
+++ auth.ts
@@ -75,6 +75,7 @@ export const config = {
       // console.log("jwt", token);
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name use the email
@@ -87,11 +88,58 @@ export const config = {
             data: { name: token.name },
           });
         }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: {
+                sessionCartId,
+              },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              // await prisma.cart.deleteMany({
+              //   where: {
+              //     userId: user.id,
+              //   },
+              // });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: {
+                  userId: user.id,
+                },
+              });
+            }
+          }
+        }
       }
 
       return token;
     },
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin\/(.*)/,
+      ];
+
+      // Get pathname from the req url object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie
@@ -103,12 +151,12 @@ export const config = {
         // Create new response and add the new headers
         const response = NextResponse.next({
           request: {
-            headers: newRequestHeaders
-          }
-        })
+            headers: newRequestHeaders,
+          },
+        });
 
         // Set newly generated sessionCartId in the response cookies
-        response.cookies.set('sessionCartId', sessionCartId);
+        response.cookies.set("sessionCartId", sessionCartId);
 
         return response;
       } else {
